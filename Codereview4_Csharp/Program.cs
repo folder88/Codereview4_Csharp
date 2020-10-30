2628ecc858ee27976ad5521a9ee409e36791dbd2
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-30 10:35:14 +0100

    test

--- Codereview4_Csharp/Program.cs
+++ Codereview4_Csharp/Program.cs
@@ -6,7 +6,7 @@ namespace Codereview4_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
