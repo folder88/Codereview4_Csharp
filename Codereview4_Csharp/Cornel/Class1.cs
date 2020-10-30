3aa26694b25aa84cde0a6e6b9ffea5815f7f3a7a
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-30 17:08:07 +0100

    17:07

---
+++ Codereview4_Csharp/Cornel/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codereview4_Csharp.Cornel
+{
+    class Class1
+    {
+
+        
+        public void method2(string name)
+
+        {
+
+            Console.WriteLine($"Hi {name} how are you today");
+            Console.WriteLine("Press 1 and Enter for: busy but ok;)\nPress 2 and Enter for:never been better :D\nPress 3 and Enter for:pretty good thanks");
+            string answer2 = Console.ReadLine();
+
+            if(answer2 == "1")
+            {
+                method2a();
+            }
+            else if(answer2 == "2")
+            {
+                method3();
+            }
+            else if (answer2 == "3")
+            {
+                method3();
+            }
+
+        }
+
+        public void method2a()
+        {
+            Console.WriteLine("Great!would you like to use our express-delivery service?\nPress 1 for:sounds good\nPress 2 for:no hurry");
+            string answer2a = Console.ReadLine();
+
+            if (answer2a == "1")
+            {
+                method4();
+            }
+            else if(answer2a == "2")
+            {
+                method3();
+            }
+        }
+        public void method3()
+
+            
+        {
+            Console.WriteLine("would you like to order some food?\nPress 1 for:not yet,just wanted to chat\nPress 2 for:yes please");
+            string answer3 = Console.ReadLine();
+            if (answer3 == "1")
+            {
+                Console.WriteLine("Have a nice day");
+            }
+           else if (answer3 == "2")
+            {
+                method4();
+            }
+
+        }
+
+        public void method4()
+        {
+            Console.WriteLine("What type of pizza would you like?\nPress 1 for:Pizza romana\nPress 2 for:Pizza diavola\nPress 3 for:Pizza tonna\nPress 5 for:Pizza formaggio\nPress 6 for:Pizza margherita");
+            string pizza = Console.ReadLine();
+            Console.WriteLine("what size?\nPress 1 for:small\nPress 2 for:big\nPress 3 for:super-big family pizza");
+            string answer5 = Console.ReadLine();
+            Console.WriteLine("Would you like to add some drinks to your order?\nPress 1 for:yes please\n2:no,thanks");
+            string answer6 = Console.ReadLine();
+            if(answer6 == "1")
+            {
+                method6a();
+            }
+            else if(answer6 == "2")
+            {
+                method7();
+            }
+
+        }
+        public void method6a() {
+            Console.WriteLine("1:Bear\n2:Cola\nJuice");
+        }
+        public void method7()
+        {
+         
+        }
+    }
+}
--- Codereview4_Csharp/Program.cs
+++ Codereview4_Csharp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Codereview4_Csharp.Cornel;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace Codereview4_Csharp
 {
@@ -6,7 +8,37 @@ namespace Codereview4_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            method3();
+        }
+
+        static void method3()
+        {
+            Console.WriteLine(" Hi whats your name? \n\n Press 1 and Enter: for entering your name \n Press 2 and Enter: for i dont talk to strangers");
+            
+            string answer = Console.ReadLine();
+
+            if(answer == "1")
+            {
+
+                Class1 task_01 = new Cornel.Class1();
+                Console.WriteLine("Whats your name?");
+                string name = Console.ReadLine();
+ 
+                task_01.method2(name);
+                
+
+                
+
+
+                
+                
+            }
+            else if(answer == "2")
+            {
+                Console.WriteLine("Have a nice Day!");
+            }
+            
+
         }
     }
 }
